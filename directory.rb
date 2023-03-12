c35f4ac9877a1abf9c57a6be44bbd09669ec9943
natashabuckham <natashaisobel@gmail.com>
2023-03-12 23:04:27 +0000

    12. loading the data from the file

--- directory.rb
+++ directory.rb
@@ -43,11 +43,21 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 # print the menu and ask the user what to do
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -67,6 +77,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
