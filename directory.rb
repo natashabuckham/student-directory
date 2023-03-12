ace3cc65269fee71638496020e252499c65fecb7
natashabuckham <natashaisobel@gmail.com>
2023-03-12 22:50:12 +0000

    11. saving the data to a file

--- directory.rb
+++ directory.rb
@@ -28,10 +28,26 @@ def print_footer
   puts "Overall we have #{@students.count} great students"
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    # create a new array with the student name and cohort
+    student_data = [student[:name], student[:cohort]]
+    # convert the array to a comma-separated string
+    csv_line = student_data.join(",")
+    # puts the argument to the file rather than the console
+    file.puts csv_line
+  end
+  file.close
+end
+
 # print the menu and ask the user what to do
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -49,6 +65,8 @@ def process(selection)
     when "2"
       # show the students
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
